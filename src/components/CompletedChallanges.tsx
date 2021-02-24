9e2529836b18ad0768ea7557c4e104fbaf06e804
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-02-24 20:37:12 -0300

    add: challange context values

--- src/components/CompletedChallanges.tsx
+++ src/components/CompletedChallanges.tsx
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
+import { ChallangesContext } from '../contexts/ChallangesContext';
 import styles from '../styles/components/CompletedChallanges.module.css';
 
 export function CompletedChallanges() {
+  const { challangesCompleted } = useContext(ChallangesContext);
+
   return (
     <div className={styles.completedChallangesContainer}>
       <span>Desafios completos</span>
-      <span>05</span>
+      <span>{challangesCompleted.toString().padStart(2, '0')}</span>
     </div>
   )
 }
