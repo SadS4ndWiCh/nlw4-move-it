7aced7f9344f87a829828eb935bf202fb6681980
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-02-25 20:29:34 -0300

    add: countdown provider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import styles from '../styles/pages/Home.module.css';
 
 import { ExperienceBar } from '../components/ExperienceBar';
@@ -5,8 +7,8 @@ import { Profile } from '../components/Profile';
 import { CompletedChallanges } from '../components/CompletedChallanges';
 import { Countdown } from '../components/Countdown';
 
-import Head from 'next/head';
 import { ChallangeBox } from '../components/ChallangeBox';
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 export default function Home() {
   return (
@@ -17,16 +19,18 @@ export default function Home() {
 
       <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallanges />
-          <Countdown />
-        </div>
-        <div>
-          <ChallangeBox />
-        </div>
-      </section>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallanges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallangeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   )
 }
