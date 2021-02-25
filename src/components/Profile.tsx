fce6ba9d6e2f037c4fba9cd9ece10d59e769d5fc
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-02-25 20:24:15 -0300

    add: get level from challanges context

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallangesContext } from '../contexts/ChallangesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallangesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/SadS4ndWiCh.png" alt="SadS4ndWiCh"/>
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>SadSAndWiCh</strong>
         <p>
           <img src="icons/level.svg" alt="Level icon"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
