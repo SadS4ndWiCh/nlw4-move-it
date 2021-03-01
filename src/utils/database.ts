ef7b00c203a1e49403404a4e4c4ad1f50a14e020
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-03-01 19:33:24 -0300

    add: database utils functions

---
+++ src/utils/database.ts
@@ -0,0 +1,23 @@
+import { MongoClient, Db } from 'mongodb';
+
+let cachedClient: any = null;
+let cachedDb: Db = null;
+
+export async function connectToDatabase() {
+  if(cachedClient && cachedDb) return { client: cachedClient, db: cachedDb };
+  const uri = process.env.MONGODB_URI;
+
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const dbName = new URL(uri).pathname.substr(1);
+
+  const db = client.db(dbName);
+
+  cachedClient = client;
+  cachedDb = db;
+  
+  return { client, db };
+}
