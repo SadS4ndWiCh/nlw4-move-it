0c4f6e700bcd4cc7c5516350f253ed2001546648
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-03-02 21:24:49 -0300

    test: some things

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,14 +2,19 @@ import { useContext } from 'react';
 import { ChallangesContext } from '../contexts/ChallangesContext';
 import styles from '../styles/components/Profile.module.css';
 
-export function Profile() {
+interface ProfileProps {
+  name: string,
+  image: string,
+}
+
+export function Profile({ name, image }: ProfileProps) {
   const { level } = useContext(ChallangesContext);
 
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/SadS4ndWiCh.png" alt="SadS4ndWiCh"/>
+      <img src={image} alt={name}/>
       <div>
-        <strong>SadSAndWiCh</strong>
+        <strong>{ name }</strong>
         <p>
           <img src="icons/level.svg" alt="Level icon"/>
           Level {level}
