2a904c0223a9da3c3568a9a6bfd8ee2572ed3765
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-03-02 21:26:27 -0300

    add: fetch user data with swr

---
+++ src/pages/app.tsx
@@ -0,0 +1,74 @@
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import { getSession, useSession } from 'next-auth/client';
+import useFetch from '../hooks/useFetch';
+
+import styles from '../styles/pages/App.module.css';
+
+import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
+import { CompletedChallanges } from '../components/CompletedChallanges';
+import { Countdown } from '../components/Countdown';
+
+import { ChallangeBox } from '../components/ChallangeBox';
+import { CountdownProvider } from '../contexts/CountdownContext';
+import { ChallangesProvider } from '../contexts/ChallangesContext';
+
+export default function DashboardPage() {
+  const [session, loading] = useSession();
+  const { data, error } = useFetch(`/user/${session?.user.name}`);
+
+  if(loading) {
+    return (<h1>loading ...</h1>)
+  }
+
+  return (
+    <>
+      { !loading && data && (
+        <ChallangesProvider
+          level={data.level}
+          currentExperience={data.currentExperience}
+          challangesCompleted={data.challangesCompleted}
+        >
+          <div className={styles.container}>
+            <Head>
+              <title>{session.user.name} | move.it</title>
+            </Head>
+    
+            <ExperienceBar />
+    
+            <CountdownProvider>
+              <section>
+                <div>
+                  <Profile name={session.user.name} image={session.user.image} />
+                  <CompletedChallanges />
+                  <Countdown />
+                </div>
+                <div>
+                  <ChallangeBox />
+                </div>
+              </section>
+            </CountdownProvider>
+          </div>
+        </ChallangesProvider>
+      ) }
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getSession(ctx);
+
+  if(!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
