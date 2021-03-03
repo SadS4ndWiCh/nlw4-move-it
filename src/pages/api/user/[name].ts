69177705e67e9ce5832ed7429daf958b1e867ba4
SadS4ndWiCh <caiocamargo007@gmail.com>
2021-03-02 21:16:30 -0300

    feat: fetch user data && update user data

--- src/pages/api/user/[name].ts
+++ src/pages/api/user/[name].ts
@@ -1,23 +1,50 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { connectToDatabase } from '../../../utils/database';
 
+import { getSession } from 'next-auth/client';
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { name } = req.query;
-
-  if(!name) {
-    return res.status(400).json({ message: 'Missing `name` data' });
-  }
-
   if(req.method === 'GET') {
+    const { name } = req.query;
+
+    if(!name) {
+      return res.status(400).json({ message: 'Missing `name` data' });
+    }
+
     const { db } = await connectToDatabase();
 
     const user = await db.collection('usersdata').findOne({ name });
 
     if(!user) {
-      return res.status(404).json({ message: `Not found user with ${name} name` });
+      return res.status(200).json({ message: `Not found user with ${name} name` });
     }
 
     return res.status(200).json(user);
+    
+  } else if(req.method === 'PUT') {
+    const session = await getSession({ req });
+    if(!session) {
+      return res.status(401).json({ message: 'You don\'t has Authorization' });
+    }
+
+    const { name } = req.query;
+    const { level, currentExperience, challangesCompleted } = req.body;
+
+    if((!level && level !== 0) || (!currentExperience && currentExperience !== 0) || (!challangesCompleted && challangesCompleted !== 0)) {
+      return res.status(400).json({ message: 'Incompleted request' });
+    }
+
+    const { db } = await connectToDatabase();
+
+    await db.collection('usersdata').updateOne({ name }, {
+      $set: {
+        level,
+        currentExperience,
+        challangesCompleted,
+      }
+    });
+
+    return res.status(200).json({ message: 'ok' });
   }
 
   res.status(400).json({ message: 'Invalid method' });
